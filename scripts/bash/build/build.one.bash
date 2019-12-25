70da220048dba327ec37c45b965e7d4e54c296dd
SDRausty <27742457+SDRausty@users.noreply.github.com>
2019-12-25 14:18:37 -0500

    commit 088186639 on 20191225

--- scripts/bash/build/build.one.bash
+++ scripts/bash/build/build.one.bash
@@ -127,7 +127,7 @@ then # load artifacts and libraries into the build process.
  	ECJENT=" -classpath $BOOTCLASSPATH "
 	printf "\\e[1;32m\\bDONE\\e[0m\\n"
 else # do not load artifacts and libraries into the build process.
-	printf "\\e[1;34m%s\\n" "See file ~/"${RDR##*/}"/.conf/LIBAUTH to automate the loading of artifacts and libraries into the compilation. The functionality of this option is being enhanced. See https://github.com/BuildAPKs/buildAPKs/issues and https://github.com/BuildAPKs/buildAPKs/pulls to familiarize yourself and improve the automation of loading artifacts and libraries into compilation.  "
+	printf "\\e[1;34m%s\\n" "File ~/"${RDR##*/}"/.conf/LIBAUTH has information regarding the automation of loading artifacts and libraries into the compilation. The functionality of this option is being enhanced. See https://github.com/BuildAPKs/buildAPKs/issues and https://github.com/BuildAPKs/buildAPKs/pulls to familiarize yourself and improve the automation of loading artifacts and libraries into compilation.  "
  	AAPTENT=""
  	ECJENT=""
 	JSJCLASSPATH=""
